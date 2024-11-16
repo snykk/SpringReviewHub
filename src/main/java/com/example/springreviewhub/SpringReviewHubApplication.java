7d2e8f6d10cb8a459143bcfecfcf3ca3c6498a81
Najib Fikri <najibfikri13@gmail.com>
2024-11-16 11:54:11 +0700

    Add starter Spring Boot project

---
+++ src/main/java/com/example/springreviewhub/SpringReviewHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.springreviewhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReviewHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReviewHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springreviewhub/SpringReviewHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springreviewhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReviewHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
